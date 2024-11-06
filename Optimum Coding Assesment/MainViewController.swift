10e19223841950095d22069f66c18e737003f289
Patrick Perdon <markpatrickperdon@gmail.com>
2024-11-06 15:10:29 +0800

    embed view controller in navigation controller and add a search controller

--- Optimum Coding Assesment/MainViewController.swift
+++ Optimum Coding Assesment/MainViewController.swift
@@ -8,27 +8,24 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    private let searchController = UISearchController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        Task {
-            do {
-                let baseURl: String =  try URLConstants.Config.value(for: .BASE_URL)
-                let apiKey: String = try URLConstants.Config.value(for: .API_KEY)
-                let test = NetworkRequestManager(baseURL: baseURl)
-                let params: [String: Any] = ["s": "avatar", "apikey": apiKey]
-                let another: MovieSearchResponse = try await test.sendNetworkRequest(withParameters: params, httpMethod: .get)
-                print(another.movies)
-                
-                
-            } catch {
-                print("error \(error)")
-            }
-        }
+        title = "Search"
+        navigationItem.searchController = searchController
     }
 
+}
+
+extension MainViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
 }
 
