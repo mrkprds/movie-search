cb96338ea5e9dc178dff53033ef1ac05fd57bd39
Patrick Perdon <markpatrickperdon@gmail.com>
2024-11-06 11:44:52 +0800

    add initial configuration file

---
+++ Optimum Coding Assesment/Configuration/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Optimum Coding Assesment
+//
+//  Created by Patrick Perdon on 06/11/2024.
+//
+
+import Foundation
+
+enum Config {
+    
+    enum BaseURL: String {
+        case BASE_URL
+        case API_KEY
+    }
+    
+}
