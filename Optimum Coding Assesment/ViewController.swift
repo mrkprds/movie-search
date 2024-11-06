eb95b5d78f293c33ce5567a222b3859390ce64ad
Patrick Perdon <markpatrickperdon@gmail.com>
2024-11-06 11:17:22 +0800

    Initial Commit

---
+++ Optimum Coding Assesment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Optimum Coding Assesment
+//
+//  Created by Patrick Perdon on 06/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Optimum Coding AssesmentTests/Optimum_Coding_AssesmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  Optimum_Coding_AssesmentTests.swift
+//  Optimum Coding AssesmentTests
+//
+//  Created by Patrick Perdon on 06/11/2024.
+//
+
+import Testing
+@testable import Optimum_Coding_Assesment
+
+struct Optimum_Coding_AssesmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
